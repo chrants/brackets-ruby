07906380eaefee859d2de60f326f81e0a7c758d4
Christian Tschoepe <tschoepe.christian@gmail.com>
2013-11-24 17:38:37 -0600

    RubyDomain.js got deleted b4 first commit. Rewrote part of what was lost

--- node/RubyDomain.js
+++ node/RubyDomain.js
@@ -1,5 +1,24 @@
 (function() {
     
+    var util            = require('util'),
+        child_process   = require('child_process'),
+        exec            = child_process.exec,
+        spawn           = child_process.spawn;
     
+    var execRuby = function(params) {
+        var ruby = spawn('ruby', (params));
+        _setRubySpawnCallbacks();
+    }
+    var _setRubySpawnCallbacks = function() {}
+    
+    var execIrb = function(params) {
+        var irb = spawn('irb', (params));
+        _setIrbSpawnCallbacks();
+    }
+    var _setIrbSpawnCallbacks = function() {}
+    
+    var init = function() {}
+    
+    exports.init = init;
         
 })();
